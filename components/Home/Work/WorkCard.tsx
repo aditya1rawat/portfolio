bf4a997bc24484c2b2701ba9a07503cb8d08c50f
Aditya Rawat <aditya1rawat@gmail.com>
2022-01-14 21:55:29 -0800

    better way to loop cards

--- components/Home/Work/WorkCard.tsx
+++ components/Home/Work/WorkCard.tsx
@@ -17,96 +17,61 @@ import { motion } from 'framer-motion';
 
 import { FiArrowUpRight, FiGithub } from 'react-icons/fi';
 
-const items = [
-	{
-		image: '/assets/images/worklogos/onpointlogo.png',
-		title: 'OnPoint',
-		text: 'A platform for teenagers to create quality media about the modern world to educate and inform the public.'
-	},
-	{
-		image: '/assets/images/worklogos/sigmoidhackslogo.png',
-		title: 'Sigmoid Hacks',
-		text: 'Machine Learning doesn’t have to be just another buzz word, make it real with Sigmoid Hacks!'
-	},
-	{
-		image: '/assets/images/worklogos/bytelogo.png',
-		title: 'Byte',
-		text: 'Reinventing how hackathons are run forever by making a more inclusive and rewarding experience and interface!'
-	}
-];
-
-const WorkCard = () => {
+export default function WorkCard({ image, text, title }: string) {
 	return (
-		<Box mt={'15px'}>
-			<SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap={8} w='full'>
-				{items.map(({ image, text, title }) => (
-					<motion.div whileHover={{ scale: 1.01 }} key={title}>
-						<GridItem
-							// borderWidth={'3px'}
-							background={
-								'linear-gradient(45deg, #D5202C 0%, #9BC53D 53.23%, #006DAA 99.99%, #FFEFEB 100%)'
-							}
-							rounded='xl'
-							p={1}
-							h='full'
-						>
-							<VStack
-								justifyContent='space-between'
-								gap={'10px'}
-								rounded='xl'
-								background={'black'}
-								p={6}
-								h='full'
-							>
-								<Box justifyContent='flex-start' w='full'>
-									<AspectRatio
-										ratio={1}
-										w={24}
-										h={24}
-										as='figure'
-									>
-										<Image
-											src={`${image}`}
-											w={'12'}
-											rounded='full'
-											alt={`${title} Logo`}
-										/>
-									</AspectRatio>
-								</Box>
+		<motion.div whileHover={{ scale: 1.01 }} key={title}>
+			<GridItem
+				// borderWidth={'3px'}
+				background={
+					'linear-gradient(45deg, #D5202C 0%, #9BC53D 53.23%, #006DAA 99.99%, #FFEFEB 100%)'
+				}
+				rounded='xl'
+				p={1}
+				h='full'
+			>
+				<VStack
+					justifyContent='space-between'
+					gap={'10px'}
+					rounded='xl'
+					background={'black'}
+					p={6}
+					h='full'
+				>
+					<Box justifyContent='flex-start' w='full'>
+						<AspectRatio ratio={1} w={24} h={24} as='figure'>
+							<Image
+								src={`${image}`}
+								w={'12'}
+								rounded='full'
+								alt={`${title} Logo`}
+							/>
+						</AspectRatio>
+					</Box>
 
-								<VStack
-									flex={1}
-									spacing={2}
-									alignItems='flex-start'
-									justifyContent='flex-end'
-								>
-									<Heading size='md'>{title}</Heading>
-									<Text fontSize='sm'>{text}</Text>
-								</VStack>
-								<Flex
-									direction={'row'}
-									gap={'2'}
-									mb='1'
-									width={'full'}
-								>
-									<IconButton
-										aria-label='Live Demo'
-										icon={<FiArrowUpRight />}
-										w='full'
-									/>
-									<IconButton
-										aria-label='Live Demo'
-										icon={<FiGithub />}
-										w='full'
-									/>
-								</Flex>
-							</VStack>
-						</GridItem>
-					</motion.div>
-				))}
-			</SimpleGrid>
-		</Box>
+					<VStack
+						flex={1}
+						spacing={2}
+						alignItems='flex-start'
+						justifyContent='flex-end'
+						color={'white'}
+					>
+						<Heading size='md'>{title}</Heading>
+						<Text fontSize='sm'>{text}</Text>
+					</VStack>
+					<Flex direction={'row'} gap={'2'} mb='1' width={'full'}>
+						<IconButton
+							aria-label='Live Demo'
+							icon={<FiArrowUpRight />}
+							w='full'
+						/>
+						<IconButton
+							aria-label='Live Demo'
+							icon={<FiGithub />}
+							w='full'
+						/>
+					</Flex>
+				</VStack>
+			</GridItem>
+		</motion.div>
 	);
-};
-
-export default WorkCard;
+}
--- components/Home/Work/index.tsx
+++ components/Home/Work/index.tsx
@@ -2,11 +2,8 @@ import {
 	Stack,
 	VStack,
 	Heading,
-	Text,
 	Box,
-	AspectRatio,
-	Image,
-	Link,
+	SimpleGrid,
 	Icon,
 	Button
 } from '@chakra-ui/react';
@@ -15,20 +12,52 @@ import NextLink from 'next/link';
 import { FiArrowRight } from 'react-icons/fi';
 import WorkCard from './WorkCard';
 
+const items = [
+	{
+		image: '/assets/images/worklogos/onpointlogo.png',
+		title: 'OnPoint',
+		text: 'A platform for teenagers to create quality media about the modern world to educate and inform the public.'
+	},
+	{
+		image: '/assets/images/worklogos/sigmoidhackslogo.png',
+		title: 'Sigmoid Hacks',
+		text: 'Machine Learning doesn’t have to be just another buzz word, make it real with Sigmoid Hacks!'
+	},
+	{
+		image: '/assets/images/worklogos/bytelogo.png',
+		title: 'Byte',
+		text: 'Reinventing how hackathons are run forever by making a more inclusive and rewarding experience and interface!'
+	}
+];
+
 export default function Work() {
 	return (
 		<Box alignItems='center' w='full' as='section' mt='24'>
 			<VStack
-				spacing={3}
+				spacing={4}
 				alignItems={{ lg: 'flex-start', base: 'center' }}
 				w='full'
 			>
 				<Heading size='xl' as='h2' display={'flex'} flexDir={'row'}>
-					Selected Work
+					Selected Works
 				</Heading>
 
-				<Stack direction={'column'} spacing={'14'} mt={'12'}>
-					<WorkCard />
+				<Stack direction={'column'} spacing={'14'}>
+					<SimpleGrid
+						columns={{ base: 1, md: 2, lg: 3 }}
+						gap={8}
+						w='full'
+					>
+						{items.map(item => {
+							return (
+								<WorkCard
+									image={item.image}
+									title={item.title}
+									text={item.text}
+								/>
+							);
+						})}
+					</SimpleGrid>
 				</Stack>
 				<NextLink href='/work' passHref>
 					<Button rightIcon={<Icon as={FiArrowRight} />}>
